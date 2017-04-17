e00412c464170ceb7270f6f159dc2834ca822ccf
Liz Bright <elizabeth.anne.bright@gmail.com>
2017-04-17 18:50:42 -0400

    added migration and seed files

--- app/Http/Controllers/PracticeController.php
+++ app/Http/Controllers/PracticeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Rych\Random\Random;
+use App\Book; # <----------- NEW
 
 class PracticeController extends Controller
 {
@@ -12,19 +13,138 @@ class PracticeController extends Controller
 * dump is a built in helper method in Laravel to quickly output information;
 * debugging purposes and not for the viewer
 */
-    public function practice1() {
-        dump('This is the first example');
-        dump([1,2,3]);
-        dd([1,2,3]);
-    //die dump, outputs data and then dies; same as die dump in php
-    // dd dies before getting to the styling
-    // good to use for debugging, dump data before getting to error
+
+    // CRUD Delete
+
+    public function practice11() {
+        # First get a book to delete
+        // looking for a book by ID 
+        $book = Book::find(11);
+
+        if(!$book) {
+            dump('Did not delete- Book not found.');
+        }
+        else {
+            $book->delete();
+            dump('Deletion complete; check the database to see if it worked...');
+        }
     }
 
-    public function practice2() {
-        dump(config('app.debug'));
-        dump(config('app'));
+
+
+    public function practice10() {
+
+        # First get a book to delete
+        $book = Book::where('author', 'LIKE', '%Scott%')->first();
+
+        // if no results
+        if(!$book) {
+            dump('Did not delete- Book not found.');
+        }
+        else {
+            $book->delete();
+            dump('Deletion complete; check the database to see if it worked...');
+        }
+
     }
+
+    // CRUD Update
+
+    public function practice9() {
+
+        # First get a book to update
+        $book = Book::where('author', 'LIKE', '%Scott%')->first();
+
+        if(!$book) {
+            dump("Book not found, can't update.");
+        }
+        else {
+
+            # Change some properties
+            $book->title = 'The Really Great Gatsby';
+            $book->published = '2025';
+
+            # Save the changes
+            $book->save();
+
+            dump('Update complete; check the database to confirm the update worked.');
+        }
+    }
+
+    // CRUD Read
+
+    public function practice8() {
+
+        $book = new Book();
+
+        // $books = $book->all();
+        // dump($books->toArray());
+
+        // where is the contraint method
+        // where -> 3 parameters (field we want, comparative operator, what looking for)
+        // LIKE = fuzzy match; % signs are wild cards
+        // get(); have some constraints and want to run the contraints and get results
+
+        //$books = $book->where('title', 'LIKE', '%Harry Potter%')->get();
+        $books = $book
+            ->where('title', 'LIKE', '%Harry Potter%')
+            ->orWhere('published', '>=', 1800)
+            //->where('published', '>=', 1998)
+            ->orderBy('created_at') // default is ascending
+            //->orderBy('created_at', 'desc') // default is ascending
+            //->first();
+            ->get();
+
+        dump($books->toArray());
+
+        /*if($books->isEmpty()) {
+            dump('No matches found');
+        }
+        else {
+            foreach($books as $book) {
+                dump($book->title);
+            }
+        }*/
+    }
+
+    // CRUD read
+
+    public function practice7() {
+
+        $book = new Book();
+
+        $books = $book->all();
+        dump($books->toArray());
+    }
+
+
+
+    // CRUD create
+
+    public function practice6() {
+
+    # Instantiate a new Book Model object
+    $newBook = new Book();
+
+        # Set the parameters
+        # Note how each parameter corresponds to a field in the table
+        $newBook->title = "Harry Potter and the Sorcerer's Stone";
+        $newBook->author = 'J.K. Rowling';
+        $newBook->published = 1997;
+        $newBook->cover = 'http://prodimage.images-bn.com/pimages/9780590353427_p0_v1_s484x700.jpg';
+        $newBook->purchase_link = 'http://www.barnesandnoble.com/w/harry-potter-and-the-sorcerers-stone-j-k-rowling/1100036321?ean=9780590353427';
+
+        # Invoke the Eloquent `save` method to generate a new row in the
+        # `books` table, with the above data
+        $newBook->save();
+
+    // includes all info; note that Eloquent is handling IDs and timestamps
+    //dump($newBook);
+
+    // includes only properties
+    dump($newBook->toArray());
+    }
+
 
     public function practice3() {
         $random = new Random();
@@ -41,6 +161,22 @@ class PracticeController extends Controller
         // character set A-Za-z0-9./
         $randomString = $random->getRandomString(8);
         dump($randomString);
+    }
+
+
+    public function practice2() {
+        dump(config('app.debug'));
+        dump(config('app'));
+    }
+
+
+    public function practice1() {
+        dump('This is the first example');
+        dump([1,2,3]);
+        dd([1,2,3]);
+    //die dump, outputs data and then dies; same as die dump in php
+    // dd dies before getting to the styling
+    // good to use for debugging, dump data before getting to error
     }
 
     public function index($n) {
--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,13 @@
 
 Route::get('/books', 'BookController@index');
 
-Route::get('/books/{title?}', 'BookController@view');
+Route::get('/books/new', 'BookController@createNewBook');
+Route::post('/books/new', 'BookController@storeNewBook');
+
+Route::get('/books/{title?}', 'BookController@show');
+
+# /routes/web.php
+Route::get('/search', 'BookController@search');
 
 /**
 * Main homepage visitors see when they visit just /
@@ -29,9 +35,9 @@ Route::get('/', 'WelcomeController');
 /**
 * This route will only set if env is configured to local
 */
-if(config('app.env') == 'local') {
-    Route::get('/logs', function(){});
-}
+#if(config('app.env') == 'local') {
+#    Route::get('/logs', function(){});
+#}
 
 /**
 * Laravel log viewer
@@ -39,6 +45,23 @@ if(config('app.env') == 'local') {
 if(config('app.env') == 'local') {
     Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 }
+
+/**
+* Temp demo of debugbar
+*/
+/**Route::get('/debugbar', function() {
+
+    $data = Array('foo' => 'bar');
+    Debugbar::info($data);
+    Debugbar::info('Current environment: '.App::environment());
+    Debugbar::error('Error!');
+    Debugbar::warning('Watch out…');
+    Debugbar::addMessage('Another message', 'mylabel');
+
+    return 'Just demoing some of the features of Debugbar';
+
+});
+*/
 
 /**
 * Practice
@@ -50,3 +73,51 @@ Route::any('/practice/{n?}', 'PracticeController@index');
 Route::get('/example', function() {
     echo Hash::make('secret123');
 });
+
+Route::get('/debug', function() {
+
+	echo '<pre>';
+
+	echo '<h1>Environment</h1>';
+	echo App::environment().'</h1>';
+
+	echo '<h1>Debugging?</h1>';
+	if(config('app.debug')) echo "Yes"; else echo "No";
+
+	echo '<h1>Database Config</h1>';
+    	echo 'DB defaultStringLength: '.Illuminate\Database\Schema\Builder::$defaultStringLength;
+    	/*
+	The following commented out line will print your MySQL credentials.
+	Uncomment this line only if you're facing difficulties connecting to the database and you
+        need to confirm your credentials.
+        When you're done debugging, comment it back out so you don't accidentally leave it
+        running on your production server, making your credentials public.
+        */
+	//print_r(config('database.connections.mysql'));
+
+	echo '<h1>Test Database Connection</h1>';
+	try {
+		$results = DB::select('SHOW DATABASES;');
+		echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
+		echo "<br><br>Your Databases:<br><br>";
+		print_r($results);
+	}
+	catch (Exception $e) {
+		echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
+	}
+
+	echo '</pre>';
+
+});
+
+if(App::environment('local')) {
+
+    Route::get('/drop', function() {
+
+        DB::statement('DROP database foobookstwo');
+        DB::statement('CREATE database foobookstwo');
+
+        return 'Dropped foobooks; created foobookstwo.';
+    });
+
+};
